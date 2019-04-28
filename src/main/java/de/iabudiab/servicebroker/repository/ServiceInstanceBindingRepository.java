c17d80aef076ea15888852acbca638498cafce5d
iska <iska.gp@gmail.com>
2019-04-29 00:07:13 +0200

    Add binding repository methods for retrieval and deleting by svc id

--- src/main/java/de/iabudiab/servicebroker/repository/ServiceInstanceBindingRepository.java
+++ src/main/java/de/iabudiab/servicebroker/repository/ServiceInstanceBindingRepository.java
@@ -1,9 +1,14 @@
 package de.iabudiab.servicebroker.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import de.iabudiab.servicebroker.model.ServiceInstanceBinding;
 
 public interface ServiceInstanceBindingRepository extends MongoRepository<ServiceInstanceBinding, String> {
 
+	List<ServiceInstanceBinding> findByServiceInstanceId(String serviceInstanceId);
+
+	void deleteByServiceInstanceId(String serviceInstanceId);
 }
